a91ddf980aff0a7b3974cbf010b2d24c53edbb9e
hachispin <ganalonrainier255@gmail.com>
2026-02-05 18:24:08 +0000

    stylua formatting

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,13 @@
 return {
-    'sainnhe/everforest',
-    lazy = false,
-    priority = 1000,
-    config = function()
-	-- Optionally configure and load the colorscheme
-	-- directly inside the plugin declaration.
-	vim.g.everforest_enable_italic = true
-	vim.cmd.colorscheme('everforest')
-    end
+	"sainnhe/everforest",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		-- Optionally configure and load the colorscheme
+		-- directly inside the plugin declaration.
+		vim.g.everforest_background = "dark"
+		vim.g.everforest_enable_italic = true
+		vim.g.everforest_better_performance = 1
+		vim.cmd.colorscheme("everforest")
+	end,
 }
