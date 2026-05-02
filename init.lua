56861d3fa8c3743c2d38328370b8b959d409cfbd
hachispin <ganalonrainier255@gmail.com>
2026-05-02 13:35:58 +0100

    clean up init.lua, switch themes, update lock
    - change stringy stuff to lua api
    - add some more comments
    - remove redundant options
    - theme: nordic -> gruvbox-material
    - fix conflict with next buffer and put from system clipboard

--- init.lua
+++ init.lua
@@ -19,58 +19,57 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- setup lazy.nvim
 require("lazy").setup({
 	spec = { { import = "plugins" } },
-	install = { colorscheme = { "nordic" } },
+	install = { colorscheme = { "gruvbox-material" } },
 	checker = { enabled = true },
 })
 
--- preferences
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.g.terminal_emulator = "wezterm"
-
--- useful stuff
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.undofile = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.updatetime = 300
-vim.o.scrolloff = 10
-vim.o.showmode = true
-vim.diagnostic.enable = true
-vim.diagnostic.config({
+vim.opt.shiftwidth = 4 -- amount to shift (<</>>)
+vim.opt.tabstop = 4 -- size of tab
+vim.o.relativenumber = true -- lines numbers shown relative to the cursor
+vim.o.undofile = true -- save undo history to file for persistence
+vim.o.smartcase = true -- case-insensitive search unless search includes uppercase characters
+vim.o.updatetime = 300 -- makes events more responsive (300ms)
+vim.o.scrolloff = 10 -- keep 10 lines above and below cursor
+vim.diagnostic.enable = true -- enable diagnostics... duh
+vim.diagnostic.config({ -- show warnings as extra ("virtual") lines
 	virtual_lines = true,
 })
 
--- lsp stuff
+-- shut up lua
 vim.lsp.config("lua_ls", { settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
 
--- easier split nav
+-- easier split nav (even though i don't use it)
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- switching buffers
-vim.keymap.set("n", "<leader>p", ":bp<CR>", { desc = "Move to previous buffer" })
-vim.keymap.set("n", "<leader>n", ":bn<CR>", { desc = "Move to next buffer" })
+-- switching buffers with <-,.->
+vim.keymap.set("n", "<leader>,", function()
+	vim.cmd.bp()
+end, { desc = "Move to previous buffer" })
+
+vim.keymap.set("n", "<leader>.", function()
+	vim.cmd.bn()
+end, { desc = "Move to next buffer" })
 
 -- view diagnostic
-vim.keymap.set("n", "<leader>v", ":lua vim.diagnostic.open_float()<CR>")
+vim.keymap.set("n", "<leader>v", function()
+	vim.diagnostic.open_float()
+end)
 
--- system clipboard
+-- yank/put with/from system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
 vim.keymap.set("n", "<leader>yy", '"+yy', { desc = "Yank line to system clipboard" })
 vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Yank to end of line to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Put from system clipboard (after)" })
 vim.keymap.set({ "n", "v" }, "<leader>P", '"+P', { desc = "Put from system clipboard (before)" })
 
--- lsp renaming but rename field is left blank, instead of having
--- the original name (which is annoying to edit since it's in cmdline)
+-- lsp rename (note: must run :wa after)
 vim.keymap.set("n", "<leader>r", function()
+	-- clear rename field (hacky)
 	vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
 		callback = function()
 			local key = vim.api.nvim_replace_termcodes("<C-u>", true, false, true)
@@ -78,5 +77,6 @@ vim.keymap.set("n", "<leader>r", function()
 			return true
 		end,
 	})
+
 	vim.lsp.buf.rename()
 end, { desc = "Rename current item under cursor with LSP" })
