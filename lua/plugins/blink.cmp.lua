38471e15e3f2fda904677df33a3f7a7b315c1520
hachispin <ganalonrainier255@gmail.com>
2026-05-14 14:06:14 +0100

    migrate to blink v2(?)

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -1,14 +1,10 @@
 return {
 	"saghen/blink.cmp",
 	-- optional: provides snippets for the snippet source
-	dependencies = { "rafamadriz/friendly-snippets" },
-
-	-- use a release tag to download pre-built binaries
-	version = "1.*",
-	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	-- build = 'cargo build --release',
-	-- If you use nix, you can build from source using latest nightly rust with:
-	-- build = 'nix run .#build-plugin',
+	dependencies = { "rafamadriz/friendly-snippets", "saghen/blink.lib" },
+	build = function()
+		require("blink.cmp").build():wait(60000)
+	end,
 
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
@@ -25,9 +21,7 @@ return {
 		-- C-k: Toggle signature help (if signature.enabled = true)
 		--
 		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = {
-			preset = "default",
-		},
+		keymap = { preset = "default" },
 
 		appearance = {
 			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
