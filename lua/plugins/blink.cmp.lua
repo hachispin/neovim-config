cfb9e48201fbde52f03c02337c9219e33dd17eb1
hachispin <ganalonrainier255@gmail.com>
2026-05-16 22:47:56 +0100

    fix that one stupid issue

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -2,7 +2,6 @@ return {
 	"saghen/blink.cmp",
 	dependencies = {
 		"saghen/blink.lib",
-		-- optional: provides snippets for the snippet source
 		"rafamadriz/friendly-snippets",
 	},
 	build = function()
@@ -27,19 +26,12 @@ return {
 		--
 		-- See :h blink-cmp-config-keymap for defining your own keymap
 		keymap = { preset = "default" },
-
-		-- (Default) Only show the documentation popup when manually triggered
-		completion = { documentation = { auto_show = false }, menu = { auto_show = true } },
-
-		-- (Default) list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		completion = {
+			documentation = { auto_show = false },
+			trigger = { show_on_insert = true, show_on_backspace = true },
+		},
 		sources = { default = { "lsp", "path", "snippets", "buffer" } },
-
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"`
-		-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "rust" },
-
-		-- please just make it OPEN
+		signature = { enabled = true },
 	},
 }
