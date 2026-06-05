2444863fb680edf83cb6b83ee5776e2ed01ce30d
hachispin <ganalonrainier255@gmail.com>
2026-06-05 02:53:02 +0100

    move unconfigured plugins to module

---
+++ lua/modules/unconfigured-plugins.lua
@@ -0,0 +1,9 @@
+vim.pack.add({
+	"https://github.com/lewis6991/gitsigns.nvim",
+	"https://github.com/nvim-mini/mini.surround",
+	"https://github.com/RRethy/nvim-treesitter-endwise",
+	"https://github.com/mrcjkb/rustaceanvim",
+	"https://github.com/tpope/vim-fugitive",
+})
+
+require("mini.surround").setup({})
