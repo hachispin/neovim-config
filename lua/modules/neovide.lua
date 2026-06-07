00c9b7362cdf8b036a2aedde41fabfcd86706fbc
hachispin <ganalonrainier255@gmail.com>
2026-06-07 04:12:13 +0100

    forgot to commit this

---
+++ lua/modules/neovide.lua
@@ -0,0 +1,59 @@
+if vim.g.neovide then
+	-- Font settings
+	-- Some font settings are in Neovide's config.toml
+	vim.g.neovide_text_gamma = 0.8
+	vim.g.neovide_text_contrast = 0.5
+
+	-- Window settings
+	vim.g.neovide_scale_factor = 1.0
+	--vim.g.neovide_padding_top = 12
+	vim.g.neovide_refresh_rate = 120 -- only applies if --no-vsync is passed
+
+	-- Cursor settings
+	vim.g.neovide_cursor_cell_color_fallback = true
+	--[[
+	vim.g.neovide_cursor_animation_length = 0.1
+    vim.g.neovide_cursor_trail_size = 0.3
+	vim.g.neovide_scroll_animation_length = 0.3
+	--]]
+
+	-- Idle can cause some issues
+	vim.g.neovide_no_idle = true
+
+	-- Zooming implementation
+	local sf = 1.125
+
+	local change_scale_factor = function(delta)
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+	end
+
+	vim.keymap.set("n", "<C-=>", function()
+		change_scale_factor(sf)
+	end)
+
+	vim.keymap.set("n", "<C-0>", function()
+		vim.g.neovide_scale_factor = 1.0
+	end)
+
+	vim.keymap.set("n", "<C-->", function()
+		change_scale_factor(1 / sf)
+	end)
+
+	-- Transparency controls
+	-- NOTE: future self may want vim.g.neovide_normal_opacity
+	local opacity_interval = 0.05
+	local minimum_opacity = 0.5
+	vim.g.neovide_opacity = 1.0
+
+	vim.keymap.set("n", "<leader>t=", function()
+		vim.g.neovide_opacity = math.min(1.0, vim.g.neovide_opacity + opacity_interval)
+	end)
+
+	vim.keymap.set("n", "<leader>t0", function()
+		vim.g.neovide_opacity = 1.0
+	end)
+
+	vim.keymap.set("n", "<leader>t-", function()
+		vim.g.neovide_opacity = math.max(minimum_opacity, vim.g.neovide_opacity - opacity_interval)
+	end)
+end
