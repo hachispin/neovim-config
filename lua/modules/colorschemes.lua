41c0e497a35fb43f9b932ae68662d48c0d925c9e
hachispin <ganalonrainier255@gmail.com>
2026-06-09 16:33:53 +0100

    delete unnecessary, add gruvbox-material

--- lua/modules/colorschemes.lua
+++ lua/modules/colorschemes.lua
@@ -2,12 +2,15 @@ vim.pack.add({ "https://github.com/xiyaowong/transparent.nvim" })
 
 vim.pack.add({ -- colorschemes
 	"https://github.com/sainnhe/everforest",
+	"https://github.com/sainnhe/gruvbox-material",
 	"https://github.com/alexvzyl/nordic.nvim",
 	"https://github.com/olivercederborg/poimandres.nvim",
 })
 
+local use_italic = false
+
 require("transparent").setup({ extra_groups = { "CursorLine" } })
-vim.g.everforest_enable_italic = true
+vim.g.everforest_enable_italic = use_italic
 vim.g.everforest_background = "hard"
 
-vim.cmd.colorscheme("everforest")
+vim.cmd.colorscheme("gruvbox-material")
