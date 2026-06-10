1a87647b82c83f60db63575f9bee268aa2143885
hachispin <ganalonrainier255@gmail.com>
2026-06-11 00:45:28 +0100

    make scrolloff more consistent, comment some more

--- init.lua
+++ init.lua
@@ -37,8 +37,8 @@ vim.diagnostic.config({
 	virtual_lines = false,
 	virtual_text = false,
 
-	-- Set the signs but don't show them in the column
-	-- (in favour of tiny-inline-diagnostic diag counts)
+	-- HACK: Set signs so that tiny-inline-diagnostic can inherit these
+	-- as signs can't be configured per severity in the plugin's config.
 	signs = {
 		text = {
 			[vim.diagnostic.severity.ERROR] = "󰅚 ",
@@ -60,7 +60,8 @@ vim.diagnostic.config({
 	},
 })
 
--- Hide diagnostic signs in column in favour of tiny-inline-diagnostic
+-- HACK: Lobotomize the sign handlers (displayed in the signcolumn) to be replaced
+-- with tiny-inline-diagnostic with the multilines + display_count options enabled.
 vim.diagnostic.handlers.signs = {
 	show = function(_, _, _, _) end,
 	hide = function(_, _) end,
@@ -123,6 +124,25 @@ vim.keymap.set({ "n", "v" }, "<leader>P", '"+P')
 vim.keymap.set({ "n", "v" }, "<leader>d", '"+d')
 vim.keymap.set("n", "<leader>dd", '"+dd')
 vim.keymap.set("n", "<leader>D", '"+D')
+
+-- NOTE: This should be replaced with scrolloffpad if it
+-- ever receives the options to replicate this behaviour.
+--
+-- Make scrolloff behave as you'd expect when approaching EOF
+vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "BufEnter" }, {
+	group = vim.api.nvim_create_augroup("ScrollOffEOF", {}),
+	callback = function()
+		local win_h = vim.api.nvim_win_get_height(0)
+		local off = math.min(vim.o.scrolloff, math.floor(win_h / 2))
+		local dist = vim.fn.line("$") - vim.fn.line(".")
+		local rem = vim.fn.line("w$") - vim.fn.line("w0") + 1
+		if dist < off and win_h - rem + dist < off then
+			local view = vim.fn.winsaveview()
+			view.topline = view.topline + off - (win_h - rem + dist)
+			vim.fn.winrestview(view)
+		end
+	end,
+})
 
 vim.api.nvim_create_user_command("Cfg", function()
 	vim.cmd.cd(vim.fn.stdpath("config"))
