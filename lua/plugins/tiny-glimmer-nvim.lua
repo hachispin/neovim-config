b56aea3cc69c8bf2c7a904d6c02fad7a912dd220
hachispin <ganalonrainier255@gmail.com>
2026-06-11 00:47:28 +0100

    add shimmer

---
+++ lua/plugins/tiny-glimmer-nvim.lua
@@ -0,0 +1,21 @@
+vim.pack.add({ "https://github.com/rachartier/tiny-glimmer.nvim" })
+
+require("tiny-glimmer").setup({
+	-- Enable/disable the plugin
+	enabled = true,
+
+	-- Automatically reload highlights when colorscheme changes
+	-- When enabled, cached highlights will be refreshed on ColorScheme autocmd
+	autoreload = true,
+
+	-- Automatic keybinding overwrites
+	overwrite = {
+
+		-- Animations
+		yank = { enabled = true },
+		search = { enabled = true },
+		paste = { enabled = true },
+		undo = { enabled = true },
+		redo = { enabled = true },
+	},
+})
