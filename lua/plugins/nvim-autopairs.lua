ecf395fa67b9a0fe4b7d44faef7ad524956de6ae
hachispin <ganalonrainier255@gmail.com>
2026-06-11 00:46:18 +0100

    add generics

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -9,4 +9,14 @@ npairs.add_rules({
 	Rule(" ", " ", { "sh", "bash", "zsh" }):with_pair(function(opts)
 		return opts.line:sub(opts.col - 1, opts.col) == "[]"
 	end):with_move(cond.done()),
+
+	Rule("<", ">"):with_pair(
+		-- regex will make it so that it will auto-pair on
+		-- `a<` but not `a <`
+		-- The `:?:?` part makes it also
+		-- work on Rust generics like `some_func::<T>()`
+		cond.before_regex("%a+:?:?$", 3)
+	):with_move(function(opts)
+		return opts.char == ">"
+	end),
 })
