63c4d25aebe7a449e82452f25f686f055f1b10c6
hachispin <ganalonrainier255@gmail.com>
2026-06-16 18:45:35 +0100

    small changes + upd

--- lua/modules/colorschemes.lua
+++ lua/modules/colorschemes.lua
@@ -1,25 +1,25 @@
 lazy_later(function()
 	vim.pack.add({
-		"https://github.com/alexvzyl/nordic.nvim", -- nordic
-		"https://github.com/olivercederborg/poimandres.nvim", -- poimandres
-		"https://github.com/savq/melange-nvim", -- melange
 		"https://github.com/alexmozaidze/palenight.nvim", -- palenight
+		"https://github.com/alexvzyl/nordic.nvim", -- nordic
 		"https://github.com/ficd0/ashen.nvim", -- ashen
+		"https://github.com/olivercederborg/poimandres.nvim", -- poimandres
+		"https://github.com/neanias/everforest-nvim", -- everforest
+	})
+
+	require("everforest").setup({
+		---Whether italics should be used for keywords and more.
+		italics = true,
+		---The contrast of line numbers, indent lines, etc. Options are `"high"` or
+		---`"low"` (default).
+		ui_contrast = "high",
 	})
 
 	require("palenight").setup({ italic = true })
 end)
 
 vim.pack.add({
-	"https://github.com/neanias/everforest-nvim", -- everforest
+	"https://github.com/savq/melange-nvim", -- melange
 })
 
-require("everforest").setup({
-	---Whether italics should be used for keywords and more.
-	italics = true,
-	---The contrast of line numbers, indent lines, etc. Options are `"high"` or
-	---`"low"` (default).
-	ui_contrast = "high",
-})
-
-vim.cmd.colorscheme("everforest")
+vim.cmd.colorscheme("melange")
